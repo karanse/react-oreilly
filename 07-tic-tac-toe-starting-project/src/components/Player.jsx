2b8dad238dde1e777f5d1e21593eb213483eea0f
Sema Karan <s.karan@catawiki.nl>
2024-09-04 15:25:05 +0200

    add players

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -9,7 +9,6 @@ export default function Player({initialName, symbol}) {
     }
 
     function handleChange(event) {
-        console.log(event)
         setPlayerName(event.target.value);
 
     }
@@ -17,13 +16,13 @@ export default function Player({initialName, symbol}) {
     let editablePlayerName =  <span className="player-name">{playerName}</span>;
 
     if (isEditing) {
-        EditablePlayerName = <input type="text" required value={playerName} onChange={handleChange} />;
+        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange} />;
     } 
 
     return (
         <li>
             <span className="player">
-                {playerName}
+                {editablePlayerName}
                 <span className="player-symbol">{symbol}</span>
             </span>
         <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
